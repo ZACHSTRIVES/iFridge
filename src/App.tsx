48e7496d1d7f52d15c14f50c8b32f7ccb72ac830
Zach <wangziqi0325@gmail.com>
2021-08-29 02:42:47 +1200

    Loading Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
 import './App.css';
 import LoginBox from './components/authPages/loginBox';
-import { Route} from "react-router";
-import { useQuery} from "@apollo/client";
+import { Route, Switch, Redirect } from "react-router";
+import { useQuery } from "@apollo/client";
 import { SELF } from "./api/queries";
 import { Self } from "./api/__generated__/Self";
-
+import ReactLoading from 'react-loading';
+import { useEffect,useState} from 'react';
+import { useHistory} from "react-router-dom";
 
 function App() {
   const { loading, error, data } = useQuery<Self>(SELF);
-  
-  console.log(loading, error, data);
+  const [showLoading, setLoading] = useState(true);
+  const history=useHistory();
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [showLoading])
+
+
+  console.log(loading, error, data);
   return (
     <div className="App flex">
- 
+      <Switch>
+     
         <Route exact path="/">
-        <LoginBox></LoginBox>
+          {(() => {
+            if (showLoading) {
+              return(
+                <ReactLoading type={"cylon"} height={200} width={100}></ReactLoading>
+              );
+            }else{
+              return(
+                data?<Redirect to="/home"></Redirect>:<LoginBox></LoginBox>
+              );
+            }
+             
+          })}
+
         </Route>
         <Route exact path="/home">
-          <a>{data?.self.gitHub}</a>
+          {showLoading?<a></a>:<a>{data?.self.gitHub}</a>}
         </Route>
-     </div>
-    
+
+        </Switch>
+    </div>
+
   );
 }
 
