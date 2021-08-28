cee7d86256f60d9e40925d17a0c5d598597b77b5
Zach <wangziqi0325@gmail.com>
2021-08-28 21:33:20 +1200

    Github OAuth

--- src/components/authPages/loginBox.tsx
+++ src/components/authPages/loginBox.tsx
@@ -1,24 +1,84 @@
-import React from "react";
+import React, {useEffect } from "react";
 import './loginPage.css';
 import logo from '../../static/img/logo.png'
 import SigninButton from "./signInButton/signinButton";
+import { useHistory, useLocation } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../../api/mutations";
 
-const LoginBox = () =>
 
-  <section className="container flex">
-    <div className="left-dev">
-      <img
-        className="logo"
-        src={logo}
-        alt="iFridge"
-      />
-    </div>
-    <div>
-      <h1>Let you know your refrigerator like the back of your hand!</h1>
-      <SigninButton></SigninButton>
-    </div>
+export interface Login_login_student {
+  __typename: "Student";
+  id: string;
+  name: string;
+  gitHub: string;
+  imageURI: string;
+}
 
-  </section>
+export interface Login_login {
+  __typename: "LoginPayload";
+  student: Login_login_student;
+  jwt: string;
+}
+
+export interface Login {
+  login: Login_login;
+}
+
+export interface LoginVariables {
+  code: string;
+}
+
+
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const LoginBox = () => {
+
+
+  const [login] = useMutation<Login>(LOGIN);
+  const query = useQuery();
+  const history = useHistory();
+
+  useEffect(() => {
+    const loginMethod = async () => {
+      const code = query.get("code");
+      if (code != null) {
+        try {
+          const { data } = await login({ variables: { code } });
+          if (data != null) {
+            localStorage.setItem("token", data.login.jwt)
+          }
+        } catch (e) {
+          console.log(e);
+        }
+        history.push('/home');
+      }
+    };
+    loginMethod();
+  }, []);
+
+
+  return (
+    <section className="container flex">
+      <div className="left-dev">
+        <img
+          className="logo"
+          src={logo}
+          alt="iFridge"
+        />
+      </div>
+      <div>
+        <h1>Let you know your refrigerator like the back of your hand!</h1>
+        <SigninButton></SigninButton>
+      </div>
+
+    </section>
+  )
+}
+
 
 
 
