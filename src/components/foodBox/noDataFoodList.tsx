66e539e637290a53ce3c4673d300fe946b0990fb
Zach <wangziqi0325@gmail.com>
2021-09-25 00:01:55 +1200

    Fixed Bugs

---
+++ src/components/foodBox/noDataFoodList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import img from "../../static/noDataFoodList.svg";
+
+
+
+const NoDataFoodList = () =>{
+   return(
+       <div>
+           <img src={img} className="noDataFoodList-img"></img>
+
+
+       </div>
+   )
+
+
+}
+
+
+
+export default NoDataFoodList;
